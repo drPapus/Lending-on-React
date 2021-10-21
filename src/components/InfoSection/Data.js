df639242925039b71d5c49f22d2f82da73cb8ad8
drPapus <antonklimovv@gmail.com>
2021-10-21 14:16:57 +0300

    init

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bunk',
+    headLine: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-5.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headLine: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-4.svg').default,
+    alt: 'men and women with phone',
+    dark: true,
+    primary: true,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headLine: 'Create an account is extremely easy',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Man at work',
+    dark: true,
+    primary: true,
+    darkText: true
+};
+
